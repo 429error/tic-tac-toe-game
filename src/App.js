c2c22ef1d9c71ec73e4bbfba1a09b6228590cc2e
dr-ayush <207762436+dr-ayush@users.noreply.github.com>
2025-08-15 17:06:27 +0530

    Initial commit: Basic Tic-tac-toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import '/App.css';
 
-function App() {
-  return (
+function App(){
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const chechWinner = (squares) => {
+    const Lines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (let line of Lines) {
+      const [a, b, c] = line;
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = chechWinner(board);
+  const isTie = !winner && board.every(square => square !== null);
+
+  const handleClick = (index) => {
+    if(board[index] || winner) return;
+    const newBoard = [...board];
+    newboard[index] = isXNext ? 'X' : 'O';
+
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setIsXNext(true);
+  };
+
+  let status;
+  if (winner) {
+    status = `Congratulations you won : ${winner}`;
+  } else if (isTie) {
+    status = " Ohh No it's a tie!";
+  } else {
+    status = `Next player: ${isXNext ? 'X' : 'O'}`;
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <h1>Tic Tac Toe</h1>
+    <p className="status">{status}</p>
+    
+    <div className="board">
+      {board.map((square, index) => (
+        <button 
+          key={index} 
+          className="square"
+          onClick={() => handleClick(index)}
         >
-          Learn React
-        </a>
-      </header>
+          {square}
+        </button>
+      ))}
     </div>
-  );
+    
+    <button onClick={resetGame} className="reset-button">
+      New Game
+    </button>
+  </div>
+);
 }
 
 export default App;
